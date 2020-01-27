803d57b7752066102e01fc556e3be4afb9d5d556
rieryn <45051404+rieryn@users.noreply.github.com>
2020-01-27 10:07:41 -0500

    second context menu

--- background.js
+++ background.js
@@ -1,11 +1,22 @@
-function injectHumbleScript() {
+function injectHumbleRevealScript() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(tabs[0].id, {file: "humble.js"});
+        chrome.tabs.executeScript(tabs[0].id, {file: "humbleReveal.js"});
+    });
+}
+function injectHumbleActivationScript() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {file: "humbleActivate.js"});
     });
 }
 
 chrome.contextMenus.create({
-  title: "Activate all Keys",
+  title: "Reveal all Keys",
   "documentUrlPatterns":["*://www.humblebundle.com/*"],
-  onclick: injectHumbleScript
+  onclick: injectHumbleRevealScript
+});
+
+chrome.contextMenus.create({
+    title: "Activate all Keys",
+    "documentUrlPatterns":["*://www.humblebundle.com/*"],
+    onclick: injectHumbleActivationScript
 });
