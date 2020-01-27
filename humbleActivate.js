a0d59f1f1c5dad11a728ad70ebadd60fa637232b
rieryn <45051404+rieryn@users.noreply.github.com>
2020-01-27 10:45:20 -0500

    new context menu script

---
+++ humbleActivate.js
@@ -0,0 +1,4 @@
+//find and click every key activation button
+   var buttons = document.getElementsByClassName(""steam-redeem-button js-steam-redeem-button"");
+    for(var i = 0; i <= buttons.length; i++)  
+       buttons[i].click();
